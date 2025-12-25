c0c899946bec40c9b8b7d604014a809a95623f81
Jesus Puentes <jopt98@gmail.com>
2025-12-24 19:52:43 -0600

    Added missing endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './entities/user.entity';
+import { getUser } from '../auth/decorators/get-user.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  @UseGuards( AuthGuard('jwt') )
+  getMe( @getUser() user: User ) {
+    return user;
+  }
 
   @Post()
   create( @Body() createUserDto: CreateUserDto ) {
